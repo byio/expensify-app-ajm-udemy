a8e35f519b3ddd51e41dfa1fef057fb22f9eab58
byio <ben@benyang.io>
2017-12-07 16:11:56 +0800

    set up weback for production: webpack -p and using production-ready source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  // entry:'./src/playground/redux-101.js',
-  // entry:'./src/playground/redux-expensify.js',
-  // entry:'./src/playground/destructuring.js',
-  // entry:'./src/playground/higher-order-components.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-    port: 1111
-  }
+module.exports = (env) => {
+  const isProd = env === 'production';
+
+  return {
+    entry: './src/app.js',
+    // entry:'./src/playground/redux-101.js',
+    // entry:'./src/playground/redux-expensify.js',
+    // entry:'./src/playground/destructuring.js',
+    // entry:'./src/playground/higher-order-components.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      port: 1111
+    }
+  };
 };
